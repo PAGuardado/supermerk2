3746f50731bbcfe9a0454604f10007fee01deb5d
Manuel Ortiz <manuel4612@hotmail.com>
2021-07-06 22:35:26 -0500

    Node no funciona

---
+++ main.js
@@ -0,0 +1,53 @@
+const { QueueClient, QueueServiceClient } = require("@azure/storage-queue");
+
+// Retrieve the connection from an environment
+// variable called AZURE_STORAGE_CONNECTION_STRING
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=teststorageaccount4queue;AccountKey=cfghQPAEPb+Zao/pW5Miz9Qni4Xg/IY37QlsqXVnRPg5cZHCC2C1YplXz5AsN+3yemkEDzcDuAyOj+akIJ2LEg==;EndpointSuffix=core.windows.net";
+
+// Create a unique name for the queue
+const queueName = "myqueue-" + '1';
+
+console.log("Creating queue: ", queueName);
+
+// Instantiate a QueueServiceClient which will be used
+// to create a QueueClient and to list all the queues
+const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
+
+// Get a QueueClient which will be used
+// to create and manipulate a queue
+const queueClient = queueServiceClient.getQueueClient(queueName);
+
+/*
+// Create the queue
+const createQueueResponse = async () => {
+      try
+      {
+        await queueClient.create()
+      }
+      catch (error) {
+        console.error(error);
+        // expected output: ReferenceError: nonExistentFunction is not defined
+        // Note - error messages will vary depending on browser
+      }
+    }
+    
+console.log(createQueueResponse())
+*/
+
+messageText = "Hola soy David 2";
+console.log("Adding message to the queue: ", messageText);
+
+// Add a message to the queue
+const response = async () => {
+    try
+    {
+      await queueClient.sendMessage(messageText);
+    }
+    catch (error) {
+      console.error(error);
+      // expected output: ReferenceError: nonExistentFunction is not defined
+      // Note - error messages will vary depending on browser
+    }
+} 
+
+console.log(response())
